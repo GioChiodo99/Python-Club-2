e39db0aa4ab0d795c9d51c3a2fd29563be71f138
Giovanni Chiodo <Giovanni.Chiodo@seattlecolleges.edu>
2022-02-25 20:17:05 -0800

    Models test commit

--- tech/models.py
+++ tech/models.py
@@ -20,6 +20,14 @@ class Product(models.Model):
     producturl=models.URLField()
     description=models.TextField()
 
+    def discountAmount(self):
+        self.discount=self.price * .05
+        return self.discount
+
+    def discountPrice(self):
+        disc=self.discountAmount()
+        self.discountedPrice=self.price-disc
+
     def __str__(self):
         return self.productname
 
